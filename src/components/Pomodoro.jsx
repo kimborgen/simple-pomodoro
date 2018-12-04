f93c4be3ce56bbbfe496f24b1ab46b942b7b009a
Kim Borgen <me@kimborgen.com>
2018-12-04 20:32:04 +0100

    cleanup, and remove useless this.tick callback

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -5,104 +5,103 @@ import './pomodoro.css'
 class Pomodoro extends Component {
   constructor(props) {
     super(props);
-	let defaultMinutes= 2;
-	let defaultSeconds= 0;
+	  let defaultMinutes= 25;
+	  let defaultSeconds= 0;
     this.state = ({
-		defaultMinutes,
-		defaultSeconds,
-		minutes: defaultMinutes,
-		seconds: defaultSeconds,
-		state: "stopped"
+		  defaultMinutes,
+		  defaultSeconds,
+		  minutes: defaultMinutes,
+		  seconds: defaultSeconds,
+		  state: "stopped"
     })
 
     // variables used in to detect doubleClick
-	this.clickCount = 0;
+	  this.clickCount = 0;
     this.singleClickTimer = '';
   }
 
   /// timer variables to string
   timerToString = () => {
-	let str = ""
-    console.log(this.state)
-	// If minutes is only one digit then prepend a 0
-	if (this.state.minutes < 10) {
-		str += "0"
-	}
-	str += this.state.minutes
+	  let str = ""
+	  // If minutes is only one digit then prepend a 0
+	  if (this.state.minutes < 10) {
+	  	str += "0"
+	  }
+	  str += this.state.minutes
 
-	str += ":"
+	  str += ":"
 
     // If seconds is only one digit then prepend a 0 
-	if (this.state.seconds < 10) {
-		str += "0"
-	}
-	str += this.state.seconds
-	return str
+	  if (this.state.seconds < 10) {
+		  str += "0"
+	  }
+	  str += this.state.seconds
+	  return str
   }
 
   /// update timer every tick
   tick = () => {
-	// check if timer is stopped or paused
-	if (this.state.state != "active") {
-        // if it is then do not execute further, thus stopping the clock
-		return
-	} else {
-		// continue clock
-		setTimeout(this.tick, 1000);
-	}
+	  // check if timer is stopped or paused
+	  if (this.state.state != "active") {
+      // if it is then do not execute further, thus stopping the clock
+		  return
+	  } else {
+		  // continue clock
+		  setTimeout(this.tick, 1000);
+	  }
 
-	let newMinutes = this.state.minutes
-	let newSeconds = this.state.seconds
+	  let newMinutes = this.state.minutes
+	  let newSeconds = this.state.seconds
 
     // if seconds are moving below 0 then wrap to 59 and decrese minutes
-	if (this.state.seconds === 0) {
-		newMinutes--
-		newSeconds = 59
-	} else {
-		newSeconds--
-	}	
+	  if (this.state.seconds === 0) {
+		  newMinutes--
+		  newSeconds = 59
+	  } else {
+		  newSeconds--
+	  }	
     
     this.setState({
-        minutes: newMinutes,
-        seconds: newSeconds
+      minutes: newMinutes,
+      seconds: newSeconds
     })
 
-	// check if timer is done
-	if (newMinutes <= 0 && newSeconds <= 0) {
-		this.setState({
-			state: "stopped"
-		})
-	}
+	  // check if timer is done
+	  if (newMinutes <= 0 && newSeconds <= 0) {
+		  this.setState({
+			  state: "stopped"
+		  })
+  	}
   }
 
 
   /// stop and reset the timer
   handleDoubleClick = () => {
-	this.setState({
-		state: "stopped",
-		minutes: this.state.defaultMinutes,
-		seconds: this.state.defaultSeconds
-	})
+    this.setState({
+      state: "stopped",
+      minutes: this.state.defaultMinutes,
+      seconds: this.state.defaultSeconds
+    })
   }	
 
   /// active or pause the timer 
   handleSingleClick = (e) => {
-	// start the timer
-	if (this.state.state === "active") {
-		this.setState({
-			state: "paused"
-		}, this.tick)
-	} else if (this.state.state === "stopped") {
-        this.setState({
-            state: "active",
-            minutes: this.state.defaultMinutes,
-            seconds: this.state.defaultSeconds
-        }, this.tick)  
+    // start the timer
+    if (this.state.state === "active") {
+      this.setState({
+        state: "paused"
+      })
+    } else if (this.state.state === "stopped") {
+      this.setState({
+        state: "active",
+        minutes: this.state.defaultMinutes,
+        seconds: this.state.defaultSeconds
+      }, this.tick)  
     } else {	
-		this.setState({
-			state: "active"
-		}, this.tick)
-	}
+      this.setState({
+        state: "active"
+      }, this.tick)
+    }
   }
 
   // Detects double clicks, execs handleSingleClick or handleDoubleClick
@@ -110,14 +109,13 @@ class Pomodoro extends Component {
   	this.clickCount++;
     if (this.clickCount === 1) {
     	this.singleClickTimer = setTimeout(function() {
-       		this.clickCount = 0;
-        	this.handleSingleClick();
-        }.bind(this), 300);
-
+     	  this.clickCount = 0;
+        this.handleSingleClick();
+      }.bind(this), 300);
     } else if (this.clickCount === 2) {
 	    clearTimeout(this.singleClickTimer);
-        this.clickCount = 0;
-        this.handleDoubleClick();
+      this.clickCount = 0;
+      this.handleDoubleClick();
     }
   }
 
